646ccc15ae3e1e09d6180ddc124b21a131d8294b
Harysson Soares <haryssonsoares@gmail.com>
2025-02-16 20:41:23 -0300

    chore: adicionando build na lib

---
+++ dist/ui/input-group.d.ts
@@ -0,0 +1,4 @@
+import React from "react";
+import { CustomProps, InputProps } from "./input";
+declare const InputGroup: React.ForwardRefExoticComponent<InputProps & CustomProps & React.RefAttributes<HTMLInputElement>>;
+export { InputGroup };
