0d44e7b98f1537094b513c3a6b6148ebee98bf6d
Harysson Soares <haryssonsoares@gmail.com>
2025-02-17 19:36:59 -0300

    feat: add carousel component

--- index.ts
+++ index.ts
@@ -5,5 +5,6 @@ export * from "./ui/accordion";
 export * from "./ui/alert-dialog";
 export * from "./ui/button";
 export * from "./ui/card";
+export * from "./ui/carousel";
 export * from "./ui/calendar";
 export * from "./ui/async-select-group";
