0e378d142867f0d986487da12a3adebb259265b0
Harysson Soares <haryssonsoares@gmail.com>
2025-02-16 22:08:18 -0300

    feat: add exports of alert dialog and button components to build project

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,5 @@ export * from "./ui/input";
 export * from "./ui/input-group";
 export * from "./ui/label";
 export * from "./ui/accordion";
+export * from "./ui/alert-dialog";
+export * from "./ui/button";
