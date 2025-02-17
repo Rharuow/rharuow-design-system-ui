cac82dde6b8cddd295a1e547d06ab9e924990dc7
Harysson Soares <haryssonsoares@gmail.com>
2025-02-16 22:27:07 -0300

    feat: add exports async-select-group and card components

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,5 @@ export * from "./ui/label";
 export * from "./ui/accordion";
 export * from "./ui/alert-dialog";
 export * from "./ui/button";
+export * from "./ui/card";
+export * from "./ui/async-select-group";
