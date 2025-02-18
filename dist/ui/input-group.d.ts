b0782f8276b92858101238179279fd414010c513
Harysson Soares <haryssonsoares@gmail.com>
2025-02-17 21:45:50 -0300

    chore: try use just ts files

--- dist/ui/input-group.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { CustomProps, InputProps } from "./input";
-declare const InputGroup: React.ForwardRefExoticComponent<InputProps & CustomProps & React.RefAttributes<HTMLInputElement>>;
-export { InputGroup };
